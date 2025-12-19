170c42a02d9702811e2401e425d041a75a354c7c
Christelle <christelletoukam1610@gmail.com>
2025-12-19 08:16:48 +0100

    Send my work for merge

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Datasets from "./scenes/Datasets";
-// import Barchart from "./Components/Barchart";
+
 import Line from "./scenes/Line";
-// import Reports from "./scenes/reports";
-// import Settings from "./scenes/settings";
-// import Analytics from "./scenes/analytics";
-// import Calendar from "./scenes/calendar";
+import Reports from "./scenes/reports";
+import Settings from "./scenes/settings";
+import Analytics from "./scenes/analytics";
+import Calendar from "./scenes/calendar";
 
 
 
@@ -27,12 +27,12 @@ return (
           <Topbar />
 
           <Routes>
-            {/* <Route path="/" element = {<Dashboard/>} /> */}
-            {/* <Route path="/analytics" element = {<Analytics/>} /> */}
-            {/* <Route path="/datasets" element = {<Datasets/>} /> */}
-            {/* <Route path="/reports" element = {<Reports/>} /> */}
-            {/* <Route path="/settings" element = {<Settings/>} /> */}
-            {/* <Route path="/calendar" element = {<Calendar/>} /> */}
+            <Route path="/" element = {<Dashboard/>} />
+            <Route path="/analytics" element = {<Analytics/>} />
+            <Route path="/datasets" element = {<Datasets/>} />
+            <Route path="/reports" element = {<Reports/>} />
+            <Route path="/settings" element = {<Settings/>} />
+            <Route path="/calendar" element = {<Calendar/>} />
             <Route path="/line" element = {<Line/>} />
 
           </Routes>
